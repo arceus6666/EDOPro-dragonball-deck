a9c54b4f6d9ba420083c6147ee5e6b243eb086fe
Daniel Mendoza <arceus6666@hotmail.com>
2023-05-15 03:24:00 -0400

    fixing

--- script/c696000004.lua
+++ script/c696000004.lua
@@ -15,7 +15,7 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(aux.EvilHeroLimit)
+	-- e1:SetValue(aux.EvilHeroLimit)
 	c:RegisterEffect(e1)
 
   --special summon on death
