ca8bf8c31949ef0dfacce33837a6e16ff1e314af
Daniel Mendoza <arceus6666@hotmail.com>
2023-05-15 02:58:24 -0400

    fixing

--- script/c696000004.lua
+++ script/c696000004.lua
@@ -7,9 +7,18 @@ function s.initial_effect(c)
   c:EnableReviveLimit()
 
   --Fusion materials
-  Fusion.AddProcMix(c, false, false, SAIYAN.XENO_VEGETA, SAIYAN.XENO_GOKU)
+  -- Fusion.AddProcMix(c, false, false, SAIYAN.XENO_VEGETA, SAIYAN.XENO_GOKU)
+  c:FusionProc(SAIYAN.XENO_VEGETA, SAIYAN.XENO_GOKU)
 
-  --special summon
+  --Special Summon condition
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.EvilHeroLimit)
+	c:RegisterEffect(e1)
+
+  --special summon on death
   local e1 = Effect.CreateEffect(c)
   e1:SetDescription(aux.Stringid(id, 0))
   e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
--- script/functions.lua
+++ script/functions.lua
@@ -1,1 +1,5 @@
 Duel.LoadScript("constants.lua")
+
+function Card.FusionProc(c, ...)
+  Fusion.AddProcMix(c, false, false, ...)
+end
