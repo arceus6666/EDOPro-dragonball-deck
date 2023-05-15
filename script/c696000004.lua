9c511201cf1026b0d8ff35c8a4a5b68b2903bc7b
Daniel Mendoza <arceus6666@hotmail.com>
2023-05-14 22:37:09 -0400

    fixing

--- script/c696000004.lua
+++ script/c696000004.lua
@@ -9,22 +9,22 @@ function s.initial_effect(c)
   --Fusion materials
   Fusion.AddProcMix(c, true, true, SAIYAN.XENO_VEGETA, SAIYAN.XENO_GOKU)
 
-  --lizard check
-  local e0 = Effect.CreateEffect(c)
-  e0:SetType(EFFECT_TYPE_SINGLE)
-  e0:SetCode(CARD_CLOCK_LIZARD)
-  e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
-  e0:SetCondition(s.lizcon)
-  e0:SetValue(1)
-  c:RegisterEffect(e0)
+  -- --lizard check
+  -- local e0 = Effect.CreateEffect(c)
+  -- e0:SetType(EFFECT_TYPE_SINGLE)
+  -- e0:SetCode(CARD_CLOCK_LIZARD)
+  -- e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+  -- e0:SetCondition(s.lizcon)
+  -- e0:SetValue(1)
+  -- c:RegisterEffect(e0)
 
-  --Special Summon condition
-  local e1 = Effect.CreateEffect(c)
-  e1:SetType(EFFECT_TYPE_SINGLE)
-  e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
-  e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-  e1:SetValue(aux.EvilHeroLimit)
-  c:RegisterEffect(e1)
+  -- --Special Summon condition
+  -- local e1 = Effect.CreateEffect(c)
+  -- e1:SetType(EFFECT_TYPE_SINGLE)
+  -- e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+  -- e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+  -- e1:SetValue(aux.EvilHeroLimit)
+  -- c:RegisterEffect(e1)
 end
 
 s.listed_names = { SAIYAN.XENO_VEGETA, SAIYAN.XENO_GOKU }
