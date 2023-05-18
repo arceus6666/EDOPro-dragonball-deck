2cdca808e9b5c3cdcb532babdb27798215b0a632
Daniel Mendoza <arceus6666@hotmail.com>
2023-05-18 02:26:42 -0400

    testing

--- script/c696000007.lua
+++ script/c696000007.lua
@@ -6,96 +6,80 @@ local s, id = GetID()
 function s.initial_effect(c)
   c:EnableReviveLimit()
 
+  c:FusionDance(696000005, 696000006)
+
   --Fusion materials
-  c:FusionProc(696000005, 696000006)
+  -- c:FusionProc(696000005, 696000006)
 
   --lizard check
-  local e0 = Effect.CreateEffect(c)
-  e0:SetType(EFFECT_TYPE_SINGLE)
-  e0:SetCode(CARD_CLOCK_LIZARD)
-  e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
-  e0:SetCondition(s.lizcon)
-  e0:SetValue(1)
-  c:RegisterEffect(e0)
+  -- local e0 = Effect.CreateEffect(c)
+  -- e0:SetType(EFFECT_TYPE_SINGLE)
+  -- e0:SetCode(CARD_CLOCK_LIZARD)
+  -- e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+  -- e0:SetCondition(s.lizcon)
+  -- e0:SetValue(1)
+  -- c:RegisterEffect(e0)
 
   -- Special Summon condition
-  local e1 = Effect.CreateEffect(c)
-  e1:SetType(EFFECT_TYPE_SINGLE)
-  e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
-  e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-  e1:SetValue(aux.MetamoranLimit)
-  c:RegisterEffect(e1)
+  -- local e1 = Effect.CreateEffect(c)
+  -- e1:SetType(EFFECT_TYPE_SINGLE)
+  -- e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+  -- e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+  -- e1:SetValue(aux.MetamoranLimit)
+  -- c:RegisterEffect(e1)
 
   --special summon on death
-  local e1 = Effect.CreateEffect(c)
-  e1:SetDescription(aux.Stringid(id, 0))
-  e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-  e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
-  e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-  e1:SetCode(EVENT_TO_GRAVE)
-  e1:SetCondition(s.condition)
-  e1:SetTarget(s.target)
-  e1:SetOperation(s.operation)
-  c:RegisterEffect(e1)
+  -- local e1 = Effect.CreateEffect(c)
+  -- e1:SetDescription(aux.Stringid(id, 0))
+  -- e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+  -- e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+  -- e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+  -- e1:SetCode(EVENT_TO_GRAVE)
+  -- e1:SetCondition(s.condition)
+  -- e1:SetTarget(s.target)
+  -- e1:SetOperation(s.operation)
+  -- c:RegisterEffect(e1)
 end
 
 s.listed_names = { CARD_FUSION_DANCE, 696000005, 696000006 }
 s.material_setcode = ARCHETYPES.SAIYAN
 s.fusion_dance = true
 
-function s.lizcon(e, tp, eg, ep, ev, re, r, rp)
-  local c = e:GetHandler()
-  return not Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(), EFFECT_SUPREME_CASTLE)
-end
+-- function s.lizcon(e, tp, eg, ep, ev, re, r, rp)
+--   local c = e:GetHandler()
+--   return not Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(), EFFECT_SUPREME_CASTLE)
+-- end
 
-function s.condition(e, tp, eg, ep, ev, re, r, rp)
-  return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
-end
+-- function s.condition(e, tp, eg, ep, ev, re, r, rp)
+--   return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+-- end
 
-function s.filter(c, e, tp, code)
-  return c:IsCode(code)
-      and c:IsCanBeSpecialSummoned(e, 0, tp, false, false, POS_FACEUP)
-end
+-- function s.filter(c, e, tp, code)
+--   return c:IsCode(code)
+--       and c:IsCanBeSpecialSummoned(e, 0, tp, false, false, POS_FACEUP)
+-- end
 
-function s.target(e, tp, eg, ep, ev, re, r, rp, chk)
-  -- if chk == 0 then
-  --   if Duel.IsPlayerAffectedByEffect(tp, CARD_BLUEEYES_SPIRIT) then return false end
-  --   if Duel.GetLocationCount(tp, LOCATION_MZONE) < 2 then return false end
-  --   local g = Duel.GetMatchingGroup(s.filter, tp, LOCATION_GRAVE, 0, nil, e, tp)
-  --   return g:GetClassCount(Card.GetCode) >= 2
-  -- end
-  if chk == 0 then
-    return
-        not Duel.IsPlayerAffectedByEffect(tp, CARD_BLUEEYES_SPIRIT)
-        and Duel.GetLocationCount(tp, LOCATION_MZONE) > 2
-        and Duel.IsExistingMatchingCard(s.filter, tp, LOCATION_GRAVE, 0, 1, nil, e, tp, 696000005)
-        and Duel.IsExistingMatchingCard(s.filter, tp, LOCATION_GRAVE, 0, 1, nil, e, tp, 696000006)
-  end
-  Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 2, tp, LOCATION_GRAVE)
-end
+-- function s.target(e, tp, eg, ep, ev, re, r, rp, chk)
+--   if chk == 0 then
+--     return
+--         not Duel.IsPlayerAffectedByEffect(tp, CARD_BLUEEYES_SPIRIT)
+--         and Duel.GetLocationCount(tp, LOCATION_MZONE) > 2
+--         and Duel.IsExistingMatchingCard(s.filter, tp, LOCATION_GRAVE, 0, 1, nil, e, tp, 696000005)
+--         and Duel.IsExistingMatchingCard(s.filter, tp, LOCATION_GRAVE, 0, 1, nil, e, tp, 696000006)
+--   end
+--   Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 2, tp, LOCATION_GRAVE)
+-- end
 
-function s.operation(e, tp, eg, ep, ev, re, r, rp)
-  if Duel.IsPlayerAffectedByEffect(tp, CARD_BLUEEYES_SPIRIT) or Duel.GetLocationCount(tp, LOCATION_MZONE) < 2 then return end
-
-  -- Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
-
-  -- local g = Duel.GetMatchingGroup(s.filter, tp, LOCATION_GRAVE, 0, nil, e, tp)
-  local g1 = Duel.GetMatchingGroup(s.filter, tp, LOCATION_GRAVE, 0, nil, e, tp, 696000005)
-  local g2 = Duel.GetMatchingGroup(s.filter, tp, LOCATION_GRAVE, 0, nil, e, tp, 696000006)
-
-  -- if #g == 0 then return end
-
-  -- local sg = aux.SelectUnselectGroup(g, e, tp, 2, 2, aux.dncheck, 1, tp, HINTMSG_SPSUMMON)
-  -- if #sg > 0 then
-  --   Duel.SpecialSummon(g, 0, tp, tp, false, false, POS_FACEUP)
-  -- end
-
-  if #g1 > 0 and #g2 > 0 then
-    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
-    local sg1 = g1:Select(tp, 1, 1, nil)
-    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
-    local sg2 = g2:Select(tp, 1, 1, nil)
-    sg1:Merge(sg2)
-    Duel.SpecialSummon(sg1, 0, tp, tp, true, false, POS_FACEUP)
-  end
-end
+-- function s.operation(e, tp, eg, ep, ev, re, r, rp)
+--   if Duel.IsPlayerAffectedByEffect(tp, CARD_BLUEEYES_SPIRIT) or Duel.GetLocationCount(tp, LOCATION_MZONE) < 2 then return end
+--   local g1 = Duel.GetMatchingGroup(s.filter, tp, LOCATION_GRAVE, 0, nil, e, tp, 696000005)
+--   local g2 = Duel.GetMatchingGroup(s.filter, tp, LOCATION_GRAVE, 0, nil, e, tp, 696000006)
+--   if #g1 > 0 and #g2 > 0 then
+--     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+--     local sg1 = g1:Select(tp, 1, 1, nil)
+--     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+--     local sg2 = g2:Select(tp, 1, 1, nil)
+--     sg1:Merge(sg2)
+--     Duel.SpecialSummon(sg1, 0, tp, tp, true, false, POS_FACEUP)
+--   end
+-- end
--- script/functions.lua
+++ script/functions.lua
@@ -6,14 +6,87 @@ end
 
 function Auxiliary.MetamoranLimit(e, se, sp, st)
   return se:GetHandler():IsCode(CARD_FUSION_DANCE)
-  -- return se:GetHandler():IsCode(CARD_DARK_FUSION)
+      -- return se:GetHandler():IsCode(CARD_DARK_FUSION)
       -- or (Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(), EFFECT_METAMOR)
-      or (Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(), EFFECT_SUPREME_CASTLE)
+      or (Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(), EFFECT_SUPREME_CASTLE) -- TODO: change card to metamor
         and st & SUMMON_TYPE_FUSION == SUMMON_TYPE_FUSION)
 end
 
--- function Auxiliary.EvilHeroLimit(e, se, sp, st)
---   return se:GetHandler():IsCode(CARD_DARK_FUSION)
---       or (Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(), EFFECT_SUPREME_CASTLE)
---         and st & SUMMON_TYPE_FUSION == SUMMON_TYPE_FUSION)
--- end
+-- Fusion Dance --
+
+local function fusionProc(c, ...)
+  Fusion.AddProcMix(c, true, true, ...)
+end
+
+local function fsodfilter(c, e, tp, code)
+  return c:IsCode(code)
+      and c:IsCanBeSpecialSummoned(e, 0, tp, false, false, POS_FACEUP)
+end
+
+local function fusionDanceSpSummon(c)
+  local e = Effect.CreateEffect(c)
+  e:SetType(EFFECT_TYPE_SINGLE)
+  e:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+  e:SetCode(EFFECT_SPSUMMON_CONDITION)
+  e:SetValue(aux.MetamoranLimit)
+  c:RegisterEffect(e)
+end
+
+local function fusionDanceLizard(c)
+  local e = Effect.CreateEffect(c)
+  e:SetType(EFFECT_TYPE_SINGLE)
+  e:SetCode(CARD_CLOCK_LIZARD)
+  e:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+  e:SetCondition(function(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    return not Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(), EFFECT_SUPREME_CASTLE)
+  end)
+  e:SetValue(1)
+  c:RegisterEffect(e0)
+  return e
+end
+
+local function fusionSummonOnDeath(c, mat1, mat2)
+  local e1 = Effect.CreateEffect(c)
+  e1:SetDescription(aux.Stringid(id, 0))
+  e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+  e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+  e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+  e1:SetCode(EVENT_TO_GRAVE)
+  e1:SetCondition(function(e, tp, eg, ep, ev, re, r, rp)
+    return e:GetHandler():IsPreviousLocation(LOCATION_ONFIELD)
+  end)
+  e1:SetTarget(function(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+      return
+          not Duel.IsPlayerAffectedByEffect(tp, CARD_BLUEEYES_SPIRIT)
+          and Duel.GetLocationCount(tp, LOCATION_MZONE) > 2
+          and Duel.IsExistingMatchingCard(fsodfilter, tp, LOCATION_GRAVE, 0, 1, nil, e, tp, mat1)
+          and Duel.IsExistingMatchingCard(fsodfilter, tp, LOCATION_GRAVE, 0, 1, nil, e, tp, mat2)
+    end
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 2, tp, LOCATION_GRAVE)
+  end)
+  e1:SetOperation(function(e, tp, eg, ep, ev, re, r, rp)
+    if Duel.IsPlayerAffectedByEffect(tp, CARD_BLUEEYES_SPIRIT) or Duel.GetLocationCount(tp, LOCATION_MZONE) < 2 then return end
+    local g1 = Duel.GetMatchingGroup(fsodfilter, tp, LOCATION_GRAVE, 0, nil, e, tp, 696000005)
+    local g2 = Duel.GetMatchingGroup(fsodfilter, tp, LOCATION_GRAVE, 0, nil, e, tp, 696000006)
+    if #g1 > 0 and #g2 > 0 then
+      Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+      local sg1 = g1:Select(tp, 1, 1, nil)
+      Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+      local sg2 = g2:Select(tp, 1, 1, nil)
+      sg1:Merge(sg2)
+      Duel.SpecialSummon(sg1, 0, tp, tp, true, false, POS_FACEUP)
+    end
+  end)
+  c:RegisterEffect(e1)
+end
+
+function Card.FusionDance(c, mat1, mat2)
+  fusionProc(c, mat1, mat2)
+  fusionDanceSpSummon(c)
+  fusionDanceLizard(c)
+  fusionSummonOnDeath(c, mat1, mat2)
+end
+
+-- Fusion Dance --
