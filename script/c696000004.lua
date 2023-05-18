cb2005b2f25e7d56b4e5c85fa2a98714729544e8
Daniel Mendoza <arceus6666@hotmail.com>
2023-05-18 00:51:32 -0400

    testing

--- script/c696000004.lua
+++ script/c696000004.lua
@@ -10,13 +10,13 @@ function s.initial_effect(c)
   -- Fusion.AddProcMix(c, false, false, SAIYAN.XENO_VEGETA, SAIYAN.XENO_GOKU)
   c:FusionProc(SAIYAN.XENO_VEGETA, SAIYAN.XENO_GOKU)
 
-  --Special Summon condition
-  -- local e1 = Effect.CreateEffect(c)
-  -- e1:SetType(EFFECT_TYPE_SINGLE)
-  -- e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
-  -- e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-  -- -- e1:SetValue(aux.EvilHeroLimit)
-  -- c:RegisterEffect(e1)
+  -- Special Summon condition
+  local e1 = Effect.CreateEffect(c)
+  e1:SetType(EFFECT_TYPE_SINGLE)
+  e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+  e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+  e1:SetValue(aux.MetamoranLimit)
+  c:RegisterEffect(e1)
 
   --special summon on death
   local e1 = Effect.CreateEffect(c)
--- script/functions.lua
+++ script/functions.lua
@@ -3,3 +3,9 @@ Duel.LoadScript("constants.lua")
 function Card.FusionProc(c, ...)
   Fusion.AddProcMix(c, false, false, ...)
 end
+
+function Auxiliary.MetamoranLimit(e, se, sp, st)
+  return se:GetHandler():IsCode(CARD_FUSION_DANCE)
+      or (Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(), EFFECT_METAMOR)
+        and st & SUMMON_TYPE_FUSION == SUMMON_TYPE_FUSION)
+end
