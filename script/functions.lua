81bb4287889f82330b1cc7e52d731b85476c4e9e
Daniel Mendoza <arceus6666@hotmail.com>
2023-05-18 00:59:25 -0400

    testing

--- script/functions.lua
+++ script/functions.lua
@@ -6,6 +6,7 @@ end
 
 function Auxiliary.MetamoranLimit(e, se, sp, st)
   return se:GetHandler():IsCode(CARD_FUSION_DANCE)
-      or (Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(), EFFECT_METAMOR)
+      -- or (Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(), EFFECT_METAMOR)
+      or (Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(), EFFECT_SUPREME_CASTLE)
         and st & SUMMON_TYPE_FUSION == SUMMON_TYPE_FUSION)
 end
