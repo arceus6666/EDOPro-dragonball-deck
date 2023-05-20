748559c8b1e0a455f8d313039f448238711a4dce
Daniel Mendoza <arceus6666@hotmail.com>
2023-05-19 21:58:30 -0400

    testing potara

--- script/c696000005.lua
+++ script/c696000005.lua
@@ -4,9 +4,11 @@ Duel.LoadScript("functions.lua")
 local s, id = GetID()
 
 function s.initial_effect(c)
-	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e1)
+	-- --Activate
+	-- local e1=Effect.CreateEffect(c)
+	-- e1:SetType(EFFECT_TYPE_ACTIVATE)
+	-- e1:SetCode(EVENT_FREE_CHAIN)
+	-- c:RegisterEffect(e1)
+  
+	aux.AddEquipProcedure(c,0)
 end
