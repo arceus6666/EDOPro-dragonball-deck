27a33a56a36b780c91a020f3efda456ea5dc92fd
Daniel Mendoza <arceus6666@hotmail.com>
2023-05-21 21:59:19 -0400

    xeno trunks and fixes

---
+++ script/c696000008.lua
@@ -0,0 +1,9 @@
+-- xeno vegeks
+
+Duel.LoadScript("functions.lua")
+local s, id = GetID()
+
+function s.initial_effect(c)
+  c:EnableReviveLimit()
+  c:PotaraFusion(SAIYAN.XENO_VEGETA, SAIYAN.XENO_TRUNKS)
+end
