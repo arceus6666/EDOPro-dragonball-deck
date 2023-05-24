d046b29d5a810ee2ccf871d1ce6c1d9be785df4a
Daniel Mendoza <arceus6666@hotmail.com>
2023-05-24 02:20:09 -0400

    tests

--- script/c696400000.lua
+++ script/c696400000.lua
@@ -6,16 +6,14 @@ local s, id = GetID()
 function s.initial_effect(c)
   c:EnableReviveLimit()
 
-  -- c:TransformSaiyanGod(SAIYAN.XENO_TRUNKS)
-
-  -- special summon condition
+  --special summon condition
   local e0 = Effect.CreateEffect(c)
   e0:SetType(EFFECT_TYPE_SINGLE)
   e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
   e0:SetCode(EFFECT_SPSUMMON_CONDITION)
   c:RegisterEffect(e0)
 
-  -- special summon
+  --special summon
   local e1 = Effect.CreateEffect(c)
   e1:SetType(EFFECT_TYPE_FIELD)
   e1:SetCode(EFFECT_SPSUMMON_PROC)
