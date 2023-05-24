995956682d269421e9388b1dfb1f00431b20759b
Daniel Mendoza <arceus6666@hotmail.com>
2023-05-24 02:57:42 -0400

    tests

--- script/c696400000.lua
+++ script/c696400000.lua
@@ -27,7 +27,7 @@ end
 
 s.listed_series = { ARCHETYPES.SAIYAN }
 
-local code = ARCHETYPES.SAIYAN + 0
+local code = RACE_SAIYAN + 0
 
 function s.cfilter(c)
   return c:IsSetCard(code) and c:IsReleasable()
