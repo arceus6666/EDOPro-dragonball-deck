14feb364b3d365d73a243d3d9b385312551f0ff8
Daniel Mendoza <arceus6666@hotmail.com>
2023-05-24 02:14:16 -0400

    tests

--- script/functions696.lua
+++ script/functions696.lua
@@ -1,10 +1,5 @@
 Duel.LoadScript("constants696.lua")
 
-function Card.IsNotCode(c, ...)
-  local codes = { ... }
-  return not c:IsCode(table.unpack(codes))
-end
-
 -- Fusion Dance --
 
 function Auxiliary.MetamoranLimit(e, se, sp, st)
@@ -167,7 +162,7 @@ end
 
 -- Potara Fusion --
 
--- Special Summon SSJ1 --
+-- Special Summon SSJ --
 
 function Card.CannotSpecialSummon(c)
   local e = Effect.CreateEffect(c)
@@ -213,4 +208,69 @@ function Card.TransformSSJ(c, base)
   c:SpecialSummonSSJ(base)
 end
 
--- Special Summon SSJ1 --
+-- Special Summon SSJ --
+
+-- God Saiyan Summon --
+
+local function rescon(base)
+  return function(sg, e, tp, mg)
+    return aux.ChkfMMZ(1)(sg, e, tp, mg)
+        and sg:IsExists(
+          function(c, sg)
+            return c:IsCode(base)
+                and sg:IsExists(Card.IsSetCard, 4, c, ARCHETYPES.SAIYAN)
+          end,
+          1, nil, sg)
+  end
+end
+
+function Card.TransformSaiyanGod(c, base)
+  local e0 = Effect.CreateEffect(c)
+  e0:SetType(EFFECT_TYPE_SINGLE)
+  e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+  e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+  c:RegisterEffect(e0)
+
+  local e1 = Effect.CreateEffect(c)
+  e1:SetType(EFFECT_TYPE_FIELD)
+  e1:SetCode(EFFECT_SPSUMMON_PROC)
+  e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+  e1:SetRange(LOCATION_HAND)
+  e1:SetCondition(function(e, c)
+    if c == nil then return true end
+    local tp = c:GetControler()
+    local rg = Duel.GetReleaseGroup(tp)
+    local g1 = rg:Filter(Card.IsCode, nil, base)
+    local g2 = rg:Filter(Card.IsSetCard, nil, ARCHETYPES.SAIYAN)
+    local g = g1:Clone()
+    g:Merge(g2)
+    return Duel.GetLocationCount(tp, LOCATION_MZONE) > -2
+        and #g1 > 0
+        and #g2 > 0
+        and #g > 1
+        and aux.SelectUnselectGroup(g, e, tp, 5, 5, rescon(base), 0)
+  end)
+  e1:SetTarget(function(e, tp, eg, ep, ev, re, r, rp, c)
+    local rg = Duel.GetReleaseGroup(tp)
+    local g1 = rg:Filter(Card.IsCode, nil, base)
+    local g2 = rg:Filter(Card.IsSetCard, nil, ARCHETYPES.SAIYAN)
+    g1:Merge(g2)
+    local sg = aux.SelectUnselectGroup(g1, e, tp, 5, 5, rescon(base), 1,
+      tp, HINTMSG_RELEASE, nil, nil, true)
+    if #sg > 0 then
+      sg:KeepAlive()
+      e:SetLabelObject(sg)
+      return true
+    end
+    return false
+  end)
+  e1:SetOperation(function(e, tp, eg, ep, ev, re, r, rp, c)
+    local g = e:GetLabelObject()
+    if not g then return end
+    Duel.Release(g, REASON_COST)
+    g:DeleteGroup()
+  end)
+  c:RegisterEffect(e1)
+end
+
+-- God Saiyan Summon --
