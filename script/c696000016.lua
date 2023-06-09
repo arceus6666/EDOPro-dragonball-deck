902043b8ccf0ed31d5db5347a81fce55c4c1496b
Daniel Mendoza <arceus6666@hotmail.com>
2023-06-09 04:52:40 -0400

    added xeno goku ssj 1,3,4

---
+++ script/c696000016.lua
@@ -0,0 +1,11 @@
+-- xeno goku ssj4
+
+Duel.LoadScript("functions696.lua")
+local s, id = GetID()
+
+function s.initial_effect(c)
+  c:EnableReviveLimit()
+  c:TransformSSJ(SAIYAN.XENO_GOKU_SSJ3)
+end
+
+s.listed_names = { SAIYAN.XENO_GOKU_SSJ3 }
